58713988ce656cdae93528615acd090e079a87af
DavidGomez27 <mmcsgomez16@gmail.com>
2017-10-05 15:13:54 -0400

    second commit

--- Week2_day4OOP/Cat.cs
+++ Week2_day4OOP/Cat.cs
@@ -8,5 +8,35 @@ namespace Week2_day4OOP
 {
     class Cat
     {
+        //STATES
+        //fields
+        private string name;
+
+        //Properties
+        public string Name
+        {
+            get { return this.name;  }
+            set { this.name = value; }
+
+        }
+
+
+        //BEHAVIORS 
+
+        //Constructors
+        public Cat()
+        {
+           //default constructor because it takes no parameters in the parenthesis
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
